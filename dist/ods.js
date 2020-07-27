037f71866df2be86479016bd52274a5c9f514c51
Dragon_Y_W <592381487@qq.com>
2020-07-27 17:12:54 +0800

    Update ods.js

--- dist/ods.js
+++ dist/ods.js
@@ -7,10 +7,10 @@ var ODS = {};
 var get_utils = function() {
 	if(typeof XLSX !== 'undefined') return XLSX.utils;
 	if(typeof module !== "undefined" && typeof require !== 'undefined') try {
-		return require('../' + 'xlsx').utils;
+		return require('./xlsx').utils;
 	} catch(e) {
-		try { return require('./' + 'xlsx').utils; }
-		catch(ee) { return require('xl' + 'sx').utils; }
+		try { return require('./xlsx').utils; }
+		catch(ee) { return require('./xlsx').utils; }
 	}
 	throw new Error("Cannot find XLSX utils");
 };
